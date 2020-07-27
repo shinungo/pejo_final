39661ebeed50be01f81c2c45127eaf1464e438ae
Markus Bolliger <markus.bolliger@gmail.com>
2020-07-27 19:30:48 +0200

    Buttons für Methoden Vorbereitet, Methodenrümpfe.

--- pejo10-data/src/main/java/ch/shinungo/pejo/controllers/ConsentIdConfirmerController.java
+++ pejo10-data/src/main/java/ch/shinungo/pejo/controllers/ConsentIdConfirmerController.java
@@ -3,7 +3,6 @@ package ch.shinungo.pejo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,27 +10,43 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ConsentIdConfirmerController {
 
-	@GetMapping({"/consentIdConfirmer", "sites/consentIdConfirmer", "/tt"})
+	@GetMapping({ "/consentIdConfirmer", "sites/consentIdConfirmer", "/tt" })
 	public String showConsentIdConfirmerHtml() {
 		log.debug("ConsentIdConfirmer direkt angesteuert");
 		return "sites/consentIdConfirmer";
 	}
 
-
 	/*
-	 * Was soll hier passieren? 
-	 * ConentIdConfirmer Html ist aufgerufen.
+	 * Was soll hier passieren? ConentIdConfirmer Html ist aufgerufen. 1.
+	 * https://api.dev.openbankingproject.ch/docs/swagger-ui.html 2. Unten:
+	 * "Post: Create Consent"
+	 * 
+	 * a: X-Request-ID b:
+	 * 
 	 * 
 	 */
-	
-	@PostMapping({"postConsent"})
+
+	@PostMapping({ "postConsent" })
 	public String postConsent() {
 		log.debug("post Consent Pressed");
-		
-		// wir müssen einen Consent erstellen. 
-		
+
+		// wir müssen einen Consent erstellen.
+
 		return "sites/consentIdConfirmer";
 	}
-	
+
+	/*
+	 * GET - ACCOUNTS: (AUS SICHT THYMELEAFE MUSS ES EIN POST SEIN.
+	 * 
+	 * X-Request-ID Consent-ID
+	 * 
+	 */
+
+	@PostMapping({ "getAccounts" })
+	public String getAccounts() {
+		log.debug("get Accounts Pressed");
+
+		return "sites/consentIdConfirmer";
+	}
 
 }
